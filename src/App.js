3f33fc29073486db3e8f4ef00b6fa611a4c25c4f
“First <Kerger06@aol.com>
2021-12-15 10:59:24 -0600

    installed materialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, { useEffect } from 'react';
+ 
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return <div className='App'>Hello World</div>;
+};
 
 export default App;
